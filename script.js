1660046dd84bf1b1ce13be2e642529fa31ddf0a7
Stonehagen <steinhagen@mailbox.org>
2022-11-30 23:07:35 +0100

    change index.html and add play and round function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function getComputerChoice() {
     // to be implemented
     let figures = ["rock", "paper", "scissors"]
@@ -6,9 +8,36 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    if (computerSelection === playerSelection) {
+        return "draw"
+    }
     
-
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You loose. Paper wraps rock."
+        } 
+        return "You won. Paper wraps rock."
+    } 
+    else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return "You loose. Scissors cut paper."
+        } 
+        return "You won. Scissors cut paper."
+    } else {
+        if (computerSelection === "rock") {
+            return "You loose. Rock crush scissors."
+        } 
+        return "You won. Rock crush scissors."
+    }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("choice: ");
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    } 
+}
+
+game();
+
