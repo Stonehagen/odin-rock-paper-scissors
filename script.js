45413980d9a5c08562a2b7ac5c8c95093c97216b
Stonehagen <steinhagen@mailbox.org>
2022-12-05 22:12:23 +0100

    add 3 buttons and eventlisterns that fire playround function

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function getComputerChoice() {
     // to be implemented
     let figures = ["rock", "paper", "scissors"]
@@ -16,28 +14,32 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === "paper") {
             return "You loose. Paper wraps rock."
         } 
-        return "You won. Paper wraps rock."
+        return "You won. Paper crushs scissors."
     } 
     else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
             return "You loose. Scissors cut paper."
         } 
-        return "You won. Scissors cut paper."
+        return "You won. paper folds rock."
     } else {
         if (computerSelection === "rock") {
             return "You loose. Rock crush scissors."
         } 
-        return "You won. Rock crush scissors."
+        return "You won. scissors cut paper."
     }
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("choice: ");
-        let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    } 
+    let playerSelection = prompt("choice: ");
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
 }
 
-game();
+const btnRsp = document.querySelectorAll('button');
+
+btnRsp.forEach( (btn) => {
+    btn.addEventListener('click', (e) => {
+        alert(playRound(e.target.id, getComputerChoice()));
+    });
+});
 
